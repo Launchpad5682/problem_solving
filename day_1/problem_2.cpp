af508fc0813a9ef15a4f9567f0a7a3b67478d3b8
launchpad5682 <launchpad5682@gmail.com>
2021-06-01 09:40:38 +0530

    problem_2 solution updated

--- day_1/problem_2.cpp
+++ day_1/problem_2.cpp
@@ -8,12 +8,14 @@ int main()
     double distance_km;
     cin >> distance_km;
     double distance_meters = distance_km * 1000;
+    double distance_inches = distance_km * 39370.07874016;
     double distance_feet = distance_km * 3280.839895;
     double distance_cm = distance_meters * 100;
 
     cout << "Distance in km: " << distance_km << endl;
     cout << "Distance in meters: " << distance_meters << endl;
-    cout << "Distance in feet: " << distance_feet << endl;
-    cout << "Distance in cm: " << distance_cm << endl;
+    cout << "Distance in inches: " << setprecision(6) << distance_inches << endl;
+    cout << "Distance in feet: " << setprecision(6) << distance_feet << endl;
+    cout << "Distance in cm: " << setprecision(6) << distance_cm << endl;
     return 0;
 }
