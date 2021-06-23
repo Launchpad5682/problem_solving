bcb9b55c0d3a796df5642e8d58a2260398fd3236
launchpad5682 <launchpad5682@gmail.com>
2021-06-23 18:08:40 +0530

    get-statistics added as template

---
+++ strings/day-2/get-statistics.py
@@ -0,0 +1,45 @@
+
+def mean(arr):
+
+    sum = 0
+    for i in arr:
+
+        sum += i
+
+    n = len(arr)
+
+    return sum/n
+
+
+def median(arr):
+
+    arr = arr.sort()
+    n = len(arr)
+    if n % 2 == 0:
+        # average of two median values 
+        pass
+
+    else: 
+        # return the median
+        pass
+
+def mode(arr):
+    # most occuring data 
+    # can use dictionary to store the values
+
+    pass
+
+
+
+def getStatistics():
+
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+
+    main()
